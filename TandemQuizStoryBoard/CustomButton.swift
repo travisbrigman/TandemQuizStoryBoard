ff0a7a591beddade7848cdb690a2db07b2e7b39d
travisbrigman <travis1000@gmail.com>
2020-11-03 08:32:33 -0600

    refactored to add buttons to view programmatically

---
+++ TandemQuizStoryBoard/CustomButton.swift
@@ -0,0 +1,44 @@
+//
+//  CustomButton.swift
+//  TandemQuizStoryBoard
+//
+//  Created by Travis Brigman on 11/2/20.
+//  Copyright © 2020 Travis Brigman. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let color = UIColor.red
+        let disabledColor = color.withAlphaComponent(0.3)
+        
+        
+        let btnFont = "Noteworthy"
+        let bthWidth = 200
+        let btnHeight = 60
+        
+        self.frame.size = CGSize(width: bthWidth, height: btnHeight)
+        self.frame.origin = CGPoint(x: (((superview?.frame.width)! / 2) - (self.frame.width / 2)), y: self.frame.origin.y)
+        
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+        self.layer.borderWidth = 3.0
+        self.layer.borderColor = color.cgColor
+        
+        self.setTitleColor(color, for: .normal)
+        self.setTitleColor(disabledColor, for: .disabled)
+        self.titleLabel?.font = UIFont(name: btnFont, size: 25)
+        self.titleLabel?.adjustsFontSizeToFitWidth = true
+        self.setTitle(self.titleLabel?.text?.capitalized, for: .normal)
+        
+    }
+    
+    func onPress() {
+        print("Pressed")
+        
+    }
+}
--- TandemQuizStoryBoard/ViewController.swift
+++ TandemQuizStoryBoard/ViewController.swift
@@ -16,7 +16,21 @@ class ViewController: UIViewController {
     let questions = DataLoader().questions.shuffled().prefix(10)
   
    
-    
+       
+
+
+    @objc func buttonAction0() {
+        checkAnswer(ansString: currentQuestion?.incorrect[0] ?? "error")
+   }
+     @objc func buttonAction1() {
+         checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
+    }
+     @objc func buttonAction2() {
+         checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
+    }
+     @objc func buttonAction3() {
+         checkAnswer(ansString: currentQuestion?.correct ?? "error")
+    }
     
     var currentQuestion: Question?
     var currentQuestionPos = 0
@@ -32,35 +46,63 @@ class ViewController: UIViewController {
     @IBOutlet var progress: UILabel!
     
     override func viewWillDisappear(_ animated: Bool) {
-        answer2.isHidden = false
+       // answer2.isHidden = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         currentQuestion = questions[0]
         
         setQuestion()
+
     }
     
 
     // Set labels and buttons for the current question
     func setQuestion() {
-        answer2.isHidden = false
+       // answer2.isHidden = false
         let incorrectAnswers = currentQuestion?.incorrect.count
         
         question.text = currentQuestion!.question
-        answer0.setTitle(currentQuestion!.incorrect[0], for: .normal)
-        answer1.setTitle(currentQuestion!.incorrect[1], for: .normal)
+        
+        let button0 = UIButton(frame: CGRect(x: 85, y: 265, width: 200, height: 60))
+        button0.setTitle(currentQuestion!.incorrect[0], for: .normal)
+        button0.setTitleColor(.systemBlue, for: .normal)
+        button0.addTarget(self, action: #selector(buttonAction0), for: .touchUpInside)
+        self.view.addSubview(button0)
+        
+        //answer0.setTitle(currentQuestion!.incorrect[0], for: .normal)
+        
+        let button1 = UIButton(frame: CGRect(x: 85, y: 285, width: 200, height: 60))
+        button1.setTitle(currentQuestion!.incorrect[1], for: .normal)
+        button1.setTitleColor(.systemBlue, for: .normal)
+        button1.addTarget(self, action: #selector(buttonAction1), for: .touchUpInside)
+        self.view.addSubview(button1)
+        
+        //answer1.setTitle(currentQuestion!.incorrect[1], for: .normal)
+        
         if let incorrectAnswers = incorrectAnswers {
             
+            let button2 = UIButton(frame: CGRect(x: 85, y: 305, width: 200, height: 60))
+            
             if incorrectAnswers > 2 {
-                answer2.setTitle(currentQuestion!.incorrect[2], for: .normal)
+                
+                button2.setTitle(currentQuestion!.incorrect[2], for: .normal)
+                button2.setTitleColor(.systemBlue, for: .normal)
+                button2.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
+                self.view.addSubview(button2)
+                //answer2.setTitle(currentQuestion!.incorrect[2], for: .normal)
             } else {
-                answer2.setTitle("", for: .normal)
-                answer2.isHidden = true
+                button2.setTitle("", for: .normal)
+                button2.isHidden = true
             }
-            answer3.setTitle(currentQuestion!.correct, for: .normal)
+            
+            let button3 = UIButton(frame: CGRect(x: 85, y: 325, width: 200, height: 60))
+            button3.setTitle(currentQuestion!.correct, for: .normal)
+            button3.setTitleColor(.systemBlue, for: .normal)
+            button3.addTarget(self, action: #selector(buttonAction3), for: .touchUpInside)
+            self.view.addSubview(button3)
+            //answer3.setTitle(currentQuestion!.correct, for: .normal)
             
         }
         
@@ -68,20 +110,20 @@ class ViewController: UIViewController {
     }
     
     // Submit an answer
-    @IBAction func submitAnswer0(_ sender: Any) {
-        checkAnswer(ansString: currentQuestion?.incorrect[0] ?? "error")
-    }
-    
-    @IBAction func submitAnswer1(_ sender: Any) {
-        checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
-    }
-    
-    @IBAction func submitAnswer2(_ sender: Any) {
-        checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
-    }
-    @IBAction func submitAnswer3(_ sender: Any) {
-        checkAnswer(ansString: currentQuestion?.correct ?? "error")
-    }
+//    @IBAction func submitAnswer0(_ sender: Any) {
+//        checkAnswer(ansString: currentQuestion?.incorrect[0] ?? "error")
+//    }
+//
+//    @IBAction func submitAnswer1(_ sender: Any) {
+//        checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
+//    }
+//
+//    @IBAction func submitAnswer2(_ sender: Any) {
+//        checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
+//    }
+//    @IBAction func submitAnswer3(_ sender: Any) {
+//        checkAnswer(ansString: currentQuestion?.correct ?? "error")
+//    }
     
     // Check if an answer is correct then load the next question
     func checkAnswer(ansString: String) {
