e5ff39162bf3543af6e03d1361ed7d7c57ef3e4f
travisbrigman <travis1000@gmail.com>
2020-11-04 08:53:11 -0600

    got stack view working. need to get answer checking working.

--- TandemQuizStoryBoard/ViewController.swift
+++ TandemQuizStoryBoard/ViewController.swift
@@ -13,24 +13,9 @@ class ViewController: UIViewController {
         viewDidLoad()
     }
     
+    var stackView = UIStackView()
+    
     let questions = DataLoader().questions.shuffled().prefix(10)
-  
-   
-       
-
-
-    @objc func buttonAction0() {
-        checkAnswer(ansString: currentQuestion?.incorrect[0] ?? "error")
-   }
-     @objc func buttonAction1() {
-         checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
-    }
-     @objc func buttonAction2() {
-         checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
-    }
-     @objc func buttonAction3() {
-         checkAnswer(ansString: currentQuestion?.correct ?? "error")
-    }
     
     var currentQuestion: Question?
     var currentQuestionPos = 0
@@ -39,70 +24,97 @@ class ViewController: UIViewController {
     
     @IBOutlet var question: UILabel!
     
-    @IBOutlet var answer0: UIButton!
-    @IBOutlet var answer1: UIButton!
-    @IBOutlet var answer2: UIButton!
-    @IBOutlet var answer3: UIButton!
     @IBOutlet var progress: UILabel!
     
     override func viewWillDisappear(_ animated: Bool) {
-       // answer2.isHidden = false
+        //button2.isHidden = false
     }
+    
+    
+    //let button0 = UIButton(frame: CGRect(x: 85, y: 265, width: 200, height: 60))
+    //let button1 = UIButton(frame: CGRect(x: 85, y: 285, width: 200, height: 60))
+    //let button2 = UIButton(frame: CGRect(x: 85, y: 305, width: 200, height: 60))
+    //let button3 = UIButton(frame: CGRect(x: 85, y: 325, width: 200, height: 60))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestion = questions[0]
-        
         setQuestion()
+        configureStackView()
+    }
+    
+    func configureStackView() {
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        addButtonsToStackView()
+        setStackViewConstraints()
+        
+    }
+    
+    func addButtonsToStackView() {
+        var allQuestions = currentQuestion?.incorrect
+        allQuestions?.append(currentQuestion?.correct ?? "no right answer")
+        allQuestions?.shuffle()
+        
+        if let allQuestions = allQuestions {
+            for question in allQuestions {
+                let button = UIButton(type: .roundedRect)
+                button.setTitle(question, for: .normal)
+
+                stackView.addArrangedSubview(button)
+                
+            }
+        }
 
     }
     
-
+    func setStackViewConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: question.bottomAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
+    }
+    
     // Set labels and buttons for the current question
     func setQuestion() {
-       // answer2.isHidden = false
+        //button2.isHidden = false
         let incorrectAnswers = currentQuestion?.incorrect.count
         
         question.text = currentQuestion!.question
         
-        let button0 = UIButton(frame: CGRect(x: 85, y: 265, width: 200, height: 60))
-        button0.setTitle(currentQuestion!.incorrect[0], for: .normal)
-        button0.setTitleColor(.systemBlue, for: .normal)
-        button0.addTarget(self, action: #selector(buttonAction0), for: .touchUpInside)
-        self.view.addSubview(button0)
+       // button0.setTitle(currentQuestion!.incorrect[0], for: .normal)
+       // button0.setTitleColor(.systemBlue, for: .normal)
+       // button0.addTarget(self, action: #selector(buttonAction0), for: .touchUpInside)
+       // self.view.addSubview(button0)
         
-        //answer0.setTitle(currentQuestion!.incorrect[0], for: .normal)
-        
-        let button1 = UIButton(frame: CGRect(x: 85, y: 285, width: 200, height: 60))
-        button1.setTitle(currentQuestion!.incorrect[1], for: .normal)
-        button1.setTitleColor(.systemBlue, for: .normal)
-        button1.addTarget(self, action: #selector(buttonAction1), for: .touchUpInside)
-        self.view.addSubview(button1)
-        
-        //answer1.setTitle(currentQuestion!.incorrect[1], for: .normal)
+        //button1.frame(forAlignmentRect: CGRect(x: 85, y: yAxis[1], width: 200, height: 60))
+        //button1.setTitle(currentQuestion!.incorrect[1], for: .normal)
+        //button1.setTitleColor(.systemBlue, for: .normal)
+        //button1.addTarget(self, action: #selector(buttonAction1), for: .touchUpInside)
+        //self.view.addSubview(button1)
         
         if let incorrectAnswers = incorrectAnswers {
             
-            let button2 = UIButton(frame: CGRect(x: 85, y: 305, width: 200, height: 60))
-            
             if incorrectAnswers > 2 {
+                //button2.frame(forAlignmentRect: CGRect(x: 85, y: yAxis[2], width: 200, height: 60))
+                //button2.setTitle(currentQuestion!.incorrect[2], for: .normal)
+                //button2.setTitleColor(.systemBlue, for: .normal)
+                //button2.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
+                //self.view.addSubview(button2)
                 
-                button2.setTitle(currentQuestion!.incorrect[2], for: .normal)
-                button2.setTitleColor(.systemBlue, for: .normal)
-                button2.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
-                self.view.addSubview(button2)
-                //answer2.setTitle(currentQuestion!.incorrect[2], for: .normal)
             } else {
-                button2.setTitle("", for: .normal)
-                button2.isHidden = true
+                //button2.setTitle("", for: .normal)
+                //button2.isHidden = true
             }
             
-            let button3 = UIButton(frame: CGRect(x: 85, y: 325, width: 200, height: 60))
-            button3.setTitle(currentQuestion!.correct, for: .normal)
-            button3.setTitleColor(.systemBlue, for: .normal)
-            button3.addTarget(self, action: #selector(buttonAction3), for: .touchUpInside)
-            self.view.addSubview(button3)
-            //answer3.setTitle(currentQuestion!.correct, for: .normal)
+            //button3.frame(forAlignmentRect: CGRect(x: 85, y: yAxis[3], width: 200, height: 60))
+            //button3.setTitle(currentQuestion!.correct, for: .normal)
+            //button3.setTitleColor(.systemBlue, for: .normal)
+            //button3.addTarget(self, action: #selector(buttonAction3), for: .touchUpInside)
+            //self.view.addSubview(button3)
             
         }
         
@@ -110,23 +122,25 @@ class ViewController: UIViewController {
     }
     
     // Submit an answer
-//    @IBAction func submitAnswer0(_ sender: Any) {
-//        checkAnswer(ansString: currentQuestion?.incorrect[0] ?? "error")
-//    }
-//
-//    @IBAction func submitAnswer1(_ sender: Any) {
-//        checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
-//    }
-//
-//    @IBAction func submitAnswer2(_ sender: Any) {
-//        checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
-//    }
-//    @IBAction func submitAnswer3(_ sender: Any) {
-//        checkAnswer(ansString: currentQuestion?.correct ?? "error")
-//    }
+
+    @objc func buttonAction(answer: String) {
+        checkAnswer(ansString: answer)
+    }
+    
+       /*
+    @objc func buttonAction1() {
+        checkAnswer(ansString: currentQuestion?.incorrect[1] ?? "error")
+    }
+    @objc func buttonAction2() {
+        checkAnswer(ansString: currentQuestion?.incorrect[2] ?? "error")
+    }
+    @objc func buttonAction3() {
+        checkAnswer(ansString: currentQuestion?.correct ?? "error")
+    }
+    */
     
     // Check if an answer is correct then load the next question
-    func checkAnswer(ansString: String) {
+    @objc func checkAnswer(ansString: String) {
         if(ansString == currentQuestion!.correct) {
             noCorrect += 1
         }
@@ -154,12 +168,12 @@ class ViewController: UIViewController {
             
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
 }
 
